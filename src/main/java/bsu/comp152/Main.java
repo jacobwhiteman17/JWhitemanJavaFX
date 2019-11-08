559f7043fe6d3651b4d50f9ab4484cec763ec15a
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-11-07 20:01:01 -0500

    I think its done. It works yay

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,3 +1,4 @@
+//Jacob Whiteman
 package bsu.comp152;
 
 import javafx.application.Application;
@@ -7,10 +8,11 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
+import javafx.stage.Stage;//these are the things I needed to import
 
 public class Main extends Application {
 
@@ -20,34 +22,35 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Canvas drawingArea = new Canvas(800,400);
-        VBox box = new VBox();
-        TextField input = new TextField();
-        Button myButton = new Button("Change Color");
-        Rectangle rect = new Rectangle();
+        Canvas drawingArea = new Canvas(800,400);//set window area
+        VBox box = new VBox();//where I will fill everything in
+        TextField input = new TextField();//where you will type
+        Button myButton = new Button("Change Color");//button
+        Rectangle rect = new Rectangle();//rect
 
-        input.setMaxWidth(400);
+        input.setMaxWidth(400);//aesthetics
 
         rect.setHeight(250);
         rect.setWidth(350);
-        rect.setFill(Paint.valueOf("Red"));
+        rect.setFill(Paint.valueOf("Red"));//making the rectangle
 
-        box.setLayoutX(400);
-        box.setSpacing(25);
-        box.getChildren().add(input);
+        box.setLayoutX(400);//center it
+        box.setSpacing(25);//space things
+        box.setBackground(Background.EMPTY);//making the background white
+        box.getChildren().add(input);//adding
         box.getChildren().add(rect);
         box.getChildren().add(myButton);
         box.getChildren().add(drawingArea);
-        EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+        EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {//creating event handler
             @Override
             public void handle(ActionEvent event) {
-                rect.setFill(Paint.valueOf(input.getText()));
+                rect.setFill(Paint.valueOf(input.getText()));//making it change the color
             }
         };
-        myButton.setOnAction(handler);
+        myButton.setOnAction(handler);//setting handler on the button
         var firstScene = new Scene(box);
         primaryStage.setScene(firstScene);
         primaryStage.setTitle("Jacob Whiteman's Project");
-        primaryStage.show();
+        primaryStage.show();//setting everything else up
     }
 }
