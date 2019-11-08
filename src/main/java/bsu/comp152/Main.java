d8807fdaf34f9dd27a57d77bccf6c38d1ba16c16
jacobwhiteman17 <j1whiteman@student.bridgew.edu>
2019-11-07 19:50:37 -0500

    It works. Nice. Maybe I'll try to make it look prettier later

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,6 +1,8 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
@@ -20,21 +22,29 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         Canvas drawingArea = new Canvas(800,400);
         VBox box = new VBox();
-        TextField text = new TextField();
+        TextField input = new TextField();
         Button myButton = new Button("Change Color");
         Rectangle rect = new Rectangle();
 
-        rect.setHeight(150);
+        input.setMaxWidth(400);
+
+        rect.setHeight(250);
         rect.setWidth(350);
         rect.setFill(Paint.valueOf("Red"));
 
         box.setLayoutX(400);
         box.setSpacing(25);
-        box.getChildren().add(text);
+        box.getChildren().add(input);
         box.getChildren().add(rect);
         box.getChildren().add(myButton);
-
         box.getChildren().add(drawingArea);
+        EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                rect.setFill(Paint.valueOf(input.getText()));
+            }
+        };
+        myButton.setOnAction(handler);
         var firstScene = new Scene(box);
         primaryStage.setScene(firstScene);
         primaryStage.setTitle("Jacob Whiteman's Project");
